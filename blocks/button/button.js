7cfed84f3db987956ba1e4234911416cf34517de
Jean-Claude Jung <jjung@adobe.com>
2023-02-17 11:29:49 +0100

    .

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,10 +1,10 @@
 function buttonTxt() {
   let text = [];
-  const button = document.querySelector('button');
+  const button = document.querySelector('.button block');
   text = button.querySelectorAll();
   const [buttonText, newText] = text;
   document.querySelector('button').text = newText;
 }
 
-const button = document.querySelector('button');
+const button = document.querySelector('.button block')[0];
 button.addEventListener('click', buttonTxt);
