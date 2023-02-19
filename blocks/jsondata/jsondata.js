2c9a97aaf6a802c9af7b8a38966d6da9dcbdca69
Jean-Claude Jung <jjung@adobe.com>
2023-02-19 15:29:06 +0100

    .

--- blocks/jsondata/jsondata.js
+++ blocks/jsondata/jsondata.js
@@ -1,3 +1,22 @@
 async function getJson(pathname) {
-  const
+  const jsn = await fetch(pathname);
+  const json = await jsn.json();
+  return json;
+}
+
+export default async function decorate(block) {
+  /* Decorate Button */
+  const btn = block.children[0].children[0];
+  btn.classList.add('btn');
+  const newDiv = document.createElement('div');
+  block.lastChild.appendChild(newDiv);
+
+
+  /* handle json data */
+  const data = getJson('/json-data.json');
+  const btnFunc = () => {
+    let a = 1;
+  };
+
+  block.addEventListner('click', btnFunc);
 }
